c7cf14c3e79a2cbf708b44039539b698eab0789e
WaspVae <waspvae@gmail.com>
2020-08-01 10:42:42 +0800

    基于 union 优化,将元素看成一个个节点,节点进行连接

--- union_find.py
+++ union_find.py
@@ -1,22 +1,49 @@
-# Quick Find
+# # Quick Find
+# class UnionFind:
+#
+#     def __init__(self, n):
+#         self.count = n
+#         self.id = [x for x in range(n)]
+#
+#     def find(self, p):
+#         assert 0 <= p < self.count
+#         return self.id[p]
+#
+#     def is_connected(self, p, q):
+#         return self.find(p) == self.find(q)
+#
+#     def union(self, p, q):
+#         p_id = self.find(p)
+#         q_id = self.find(q)
+#         if q_id == p_id:
+#             return
+#         for i in range(self.count):
+#             if self.id[i] == p_id:
+#                 self.id[i] = q_id
+
+# Quick Union
+
+
 class UnionFind:
 
     def __init__(self, n):
         self.count = n
-        self.id = [x for x in range(n)]
+        self.parent = [x for x in range(n)]
 
     def find(self, p):
         assert 0 <= p < self.count
-        return self.id[p]
+        while p != self.parent[p]:
+            p = self.parent[p]
+        return p
 
     def is_connected(self, p, q):
         return self.find(p) == self.find(q)
 
     def union(self, p, q):
-        p_id = self.find(p)
-        q_id = self.find(q)
-        if q_id == p_id:
+        p_root = self.find(p)
+        q_root = self.find(q)
+
+        if p_root == q_root:
             return
-        for i in range(self.count):
-            if self.id[i] == p_id:
-                self.id[i] = q_id
+
+        self.parent[p_root] = q_root
