179eeaf7fec15943cdf2e11918606e1f2c727c8c
WaspVae <waspvae@gmail.com>
2020-08-02 22:12:14 +0800

    LeetCode-27-remove-element(优化,遇到目标元素直接删除)

--- LeetCode/27_remove_element.py
+++ LeetCode/27_remove_element.py
@@ -1,12 +1,8 @@
 class Solution:
     def remove_element(self, nums, val):
-        k = 0  # nums 中,[0...k)的元素中不包含 val 元素
-        for i in range(len(nums)):
-            if nums[i] != val:
-                if i != k:
-                    nums[i], nums[k] = nums[k], nums[i]
-                    k += 1
-                else:
-                    k += 1
-        del nums[k:]
+        # 由于遇到目标元素会直接删除改元素,因此采用倒序遍历
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] == val:
+                del nums[i]
+        return len(nums)
 
