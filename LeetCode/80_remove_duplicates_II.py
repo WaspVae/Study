e4547ec2b41c61482df174f9ac3bc409016365c8
WaspVae <waspvae@gmail.com>
2020-08-03 00:04:09 +0800

    LeetCode-80-remove-duplicate(有序数组最多保留两个重复元素,使用双指针)

--- LeetCode/80_remove_duplicates_II.py
+++ LeetCode/80_remove_duplicates_II.py
@@ -1,9 +1,15 @@
 class Solution:
     def remove_duplicates(self, nums):
-        k = 0  # nums 中,[0...k]的元素均为满足要求的元素
-        for item in nums:
-            # 一直维护 k 的值,使[0...k]的元素均为满足要求的元素
-            if k < 2 or item != nums[k - 2]:
-                nums[k] = item
-                k += 1
-        return k
+        # sp是慢指针,fp 是快指针
+        # 快指针：遍历整个数组；
+        # 慢指针：记录可以覆写数据的位置；
+        # 题目中规定每个元素最多出现两次，因此，应检查快指针指向的元素和慢指针指针所指向单元的前一个元素是否相等。
+        # 相等则不更新慢指针，只更新快指针；不相等时，先将慢指针后移一位，再将快指针指向的元素覆写入慢指针指向的单元，最后更新快指针
+        sp = 1
+        for fp in range(2, len(nums)):
+            if nums[sp - 1] != nums[fp]:
+                sp += 1
+                nums[sp] = nums[fp]
+                fp += 1
+        return sp + 1
+
