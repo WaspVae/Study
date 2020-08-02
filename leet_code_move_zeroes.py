6b02f231aafd3af34fb1a40227dc2e05f8916eab
WaspVae <waspvae@gmail.com>
2020-08-02 21:35:38 +0800

    LeetCode-283-move-zeroes(优化:不使用额外辅助空间,直接交换元素位置,且针对数组中没有 0 时的进行优化)

--- leet_code_move_zeroes.py
+++ leet_code_move_zeroes.py
@@ -7,5 +7,8 @@ class Solution:
         # 遍历到第 i 个元素后,保证[0...i]中所有非 0 元素都按序排列在[0...k)中
         for i in range(len(nums)):
             if nums[i]:
-                nums[k], nums[i] = nums[i], nums[k]
-                k += 1
+                if i != k:
+                    nums[k], nums[i] = nums[i], nums[k]
+                    k += 1
+                else:
+                    k += 1
