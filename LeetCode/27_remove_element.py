fbc9a800d1ff91efef5793d56d2c5d6bf05034fe
WaspVae <waspvae@gmail.com>
2020-08-03 23:05:50 +0800

    LeetCode-27-remove-element

--- LeetCode/27_remove_element.py
+++ LeetCode/27_remove_element.py
@@ -5,4 +5,11 @@ class Solution:
             if nums[i] == val:
                 del nums[i]
         return len(nums)
+        # 方法二:将目标元素直接移到末尾
+        # k = 0  # [0...k)表示非 0 元素
+        # for i in range(len(nums)):
+        #     if nums[i] != val:
+        #         nums[i], nums[k] = nums[k], nums[i]
+        #         k += 1
+        # return k
 
