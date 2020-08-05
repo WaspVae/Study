ea458742eda4d6a61b8b5b5932fac942a082cb44
WaspVae <waspvae@gmail.com>
2020-08-05 23:15:02 +0800

    LeetCode-125-valid-palindrome

---
+++ LeetCode/125_valid_palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def valid_palindrome(self, s):
+        import re
+        s = ''.join(re.findall('[A-Za-z0-9]', s.lower()))
+        if not len(s):
+            return True
+        i = 0
+        j = len(s) - 1
+        while i <= j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return False
+        return True
