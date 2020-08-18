7a5bfc1df386c27b6bf1db5054b22af5f16e6d46
WaspVae <waspvae@gmail.com>
2020-08-18 20:32:43 +0800

    77

--- LeetCode/77_combinations.py
+++ LeetCode/77_combinations.py
@@ -12,7 +12,11 @@ class Solution:
         if len(c) == k:
             res.append(c[:])
             return
-        for i in range(start, n + 1):
+        # for i in range(start, n + 1):
+        # 进行减枝优化
+        # 组合 c 中还有 k-len(c) 个空位,所以 [i...n] 中至少要有 k - len(c) 个元素
+        # i 最多为 n - (k - len(c)) + 1
+        for i in range(start, n - (k - len(c)) + 2):
             c.append(i)
             self.generate_combination(n, k, i + 1, c, res)
             c.pop()
