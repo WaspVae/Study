b4211d4f071207168991a2deae9dd21e660940f2
WaspVae <waspvae@gmail.com>
2020-08-21 11:52:36 +0800

    优化

--- LeetCode/198_house_robber.py
+++ LeetCode/198_house_robber.py
@@ -26,8 +26,15 @@ class Solution:
         #         memo[i] = max(memo[i], nums[j] + (memo[j + 2] if j + 2 < n else 0))
         # return memo[0]
         # memo[i] 表示考虑抢劫 nums[0...i]这个范围内的所有房子所能获得的最大收益
+        # memo[0] = nums[0]
+        # for i in range(1, n):
+        #     for j in range(i, -1, -1):
+        #         memo[i] = max(memo[i], nums[j] + (memo[j - 2] if j - 2 >= 0 else 0))
+        # return memo[n - 1]
+        # 状态转移方程  dp[i]=max(dp[i−2]+nums[i],dp[i−1])
         memo[0] = nums[0]
-        for i in range(1, n):
-            for j in range(i, -1, -1):
-                memo[i] = max(memo[i], nums[j] + (memo[j - 2] if j - 2 >= 0 else 0))
-        return memo[n - 1]
+        memo[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            memo[i] = max(memo[i-2] + nums[i], memo[i-1])
+        return memo[n-1]
+
